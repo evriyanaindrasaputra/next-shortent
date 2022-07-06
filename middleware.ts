aaa4276a625826b26dabc319a219e271e941d7d1
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-07-06 20:33:43 +0700

    feat: middleware nextjs 12.2

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  console.log(req.nextUrl.pathname)
+  if (
+    req.nextUrl.pathname.startsWith("/api/") ||
+    req.nextUrl.pathname.startsWith("/_next/") ||
+    req.nextUrl.pathname === "/" ||
+    req.nextUrl.pathname === "/favicon.ico"
+  ) {
+    return NextResponse.next()
+  }
+  const slug = req.nextUrl.pathname.split("/").pop();
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+  if (slugFetch.status === 404) {
+  return NextResponse.redirect(new URL(req.nextUrl.origin, req.url))
+  }
+  const data = await slugFetch.json();
+  if (data?.url) {
+    return NextResponse.redirect(new URL(data.url, req.url))
+  }
+}
