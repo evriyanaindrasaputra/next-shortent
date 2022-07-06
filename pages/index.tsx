6fce58cddc21b543e9fd63f4f0b59e5ab58738c2
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-07-06 16:19:56 +0700

    feat: trpc ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { trpc } from '~/lib/trpc'
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(['shorten.all-slug'])
+  if(isLoading){
+    return <p>loading ....</p>
+  }
   return (
     <div>
-      <h1 className=' text-yellow-400 text-5xl'> Hello Ts</h1>
+      <h1 className=' text-yellow-400 text-5xl'>
+        {data?.map(item => (
+          <span key={item.slug}>
+            <a href={`/${item.slug}`}>{item.slug}</a>
+          </span>
+        ))}
+      </h1>
     </div>
   )
 }
--- server/route/shorten.router.ts
+++ server/route/shorten.router.ts
@@ -1,12 +1,34 @@
 import { createRouter } from "../createRouter";
 import * as trpc from '@trpc/server'
 import {slugCheckSchema , createSlugSchema} from '~/server/schema/shorten.schema'
+import { prisma } from "~/lib/prisma";
 
 export const shortenRouter = createRouter()
+.query('all-slug', {
+  async resolve({ctx}){
+    return await prisma.shortLink.findMany(
+      {
+        select : {
+          slug: true
+        }
+      }
+    )
+  }
+})
 .query('slug-check', {
   input : slugCheckSchema,
   async resolve({ctx, input}){
     // logic for checking slug 
+    try {
+      const count = await prisma.shortLink.count({
+        where: {
+          slug : input.slug
+        }
+      })
+      return {used : count > 0}
+    } catch (error) {
+      
+    }
   }
 })
 .mutation('create-slug', {
