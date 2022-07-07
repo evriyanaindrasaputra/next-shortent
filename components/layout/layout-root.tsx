e951202b0552dc97c3b564ed44e0cfa0ddce6cc5
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-07-07 14:16:55 +0700

    feat: add layout root

---
+++ components/layout/layout-root.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export type LayoutRootProps = React.ComponentPropsWithoutRef<'div'>;
+
+export const LayoutRoot = React.forwardRef<HTMLDivElement, LayoutRootProps>(
+  ({ style, children, ...rest }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className='flex flex-col w-full min-h-screen bg-gray-100'
+        style={style}
+        {...rest}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+LayoutRoot.displayName = 'LayoutRoot';
