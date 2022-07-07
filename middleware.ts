37baed7e112818f677196d48c55bc76841be38a1
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-07-07 14:16:26 +0700

    feat: add reach hook form and update schema

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  console.log(req.nextUrl.pathname)
   if (
     req.nextUrl.pathname.startsWith("/api/") ||
     req.nextUrl.pathname.startsWith("/_next/") ||
